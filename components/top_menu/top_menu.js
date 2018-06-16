358d1b914d200e47be8423805f0011220ce43b6e
BrianHCombes <briancombes59@yahoo.com>
2018-06-16 09:54:06 -0600

    Mostly adding pictures and words to templates, changed index.html to all click (no mouseover) for hyper menu

--- components/top_menu/top_menu.js
+++ components/top_menu/top_menu.js
@@ -7,26 +7,31 @@ angular.module("viewNav")
         templateUrl: 'components/top_menu/intro/intro.html',
        
         controller: ['GetSet', function(GetSet){
-            var vpWidth = GetSet.getViewWidth();
+            var viewWidth = GetSet.getViewWidth();
                 
             var introSelf = this;
             
-            introSelf.test = "This is a controller Test and the view width is: " + vpWidth + "px";
+            introSelf.test = "This is a controller Test and the view width is: " + viewWidth + "px";
             
-            introSelf.fontsize1 = "font-size:" + ((0.00222)*(vpWidth) + 0.40000) + "em";
+            introSelf.fontsize1 = "font-size:" + ((0.00222)*(viewWidth)+0.40000) + "em";
             
-            introSelf.fontsize2 = "font-size:" + ((0.00333)*(vpWidth) + -0.10000) + "em";
-            introSelf.fontsize3 = "font-size:" + ((0.00123)*(vpWidth) + 0.30556) + "em";
-            introSelf.fontsize4 = "font-size:" + ((0.00062)*(vpWidth) + 0.52778) + "em";
+            introSelf.fontsize2 = "font-size:" + ((0.00111)*(viewWidth)+0.70000) + "em";
+            introSelf.fontsize3 = "font-size:" + ((0.00123)*(viewWidth)+0.30556) + "em";
+            introSelf.fontsize4 = "font-size:" + ((0.00062)*(viewWidth)+0.52778) + "em";
+            introSelf.fontsize5 = "font-size:" + ((0.00086)*(viewWidth)+0.48889) + "em";
+            introSelf.fontsize6 = "font-size:" + ((0.00123)*(viewWidth)+0.55556) + "em";
             
             
-            introSelf.width1 = "width:" + ((-0.02469)*(vpWidth) + 68.88889) + "%";
-            introSelf.width2 = "width:" + ((0.02469)*(vpWidth) + 31.11111) + "%";
+            
+            introSelf.width1 = "width:" + ((-0.02469)*(viewWidth)+58.88889) + "%";
+            introSelf.width2 = "width:" + ((0.02469)*(viewWidth)+41.11111) + "%";
             
             introSelf.newWindow = function(){
                 //alert();
                 //var myWindow = window.open("https://www.tarptiedown.com", "WTF2", "width=600,height=400");
-                var myWindow = window.open("components/top_menu/intro/TTD-Logo-1200x225-08-07-2017.png", "WTF2", "width=600,height=400");
+                //var myWindow = window.open("components/top_menu/intro/TTD-Logo-1200x225-08-07-2017.png", "WTF2", "width=600,height=400,left=285");
+                var left = viewWidth/2-300;
+                var myWindow = window.open("components/top_menu/intro/navigate.html", "", "width=600,height=400,top=100,left="+left);
             };
             
             
