6a53de8ab26ff8d47e77d9879b76aed8650ecaaf
BrianHCombes <briancombes59@yahoo.com>
2018-08-14 22:28:44 -0600

    content add/edit and configure of view06 for Keeper

--- components/components04_07/components04_07.js
+++ components/components04_07/components04_07.js
@@ -113,10 +113,24 @@ angular.module('viewNav')
             self06.rs2fontsize2 = "font-size:" + ((0.00154)*(viewWidth)+0.19444) + "em";
             
             // AR Response Section 3
-            self06.rs3positiontop1 = "top:" + ((0.04938)*(viewWidth)-7.77778) + "px";
-            self06.rs3positionleft1 = "left:" + ((0.01235)*(viewWidth)+70.55556)    + "%";  // AR_85_75
-            self06.rs3width3 = "width:" + ((0.09877)*(viewWidth)+34.44444) + "px";          // AR_150_70
-            self06.rs3fontsize4 = "font-size:" + ((0.00068)*(viewWidth)+0.45556) + "em";
+            self06.rs3fontsize1 = "font-size:" + ((0.00095)*(viewWidth)+0.38778) + "em";        /* AR_1.50_0.73 */
+            self06.rs3top1 = "top:" + ((0.01852)*(viewWidth)+8.33333) + "px";                   /* AR_30.00_15.00 */      
+            self06.rs3marginleft1= "margin-left:" + ((0.01852)*(viewWidth)-1.66667) + "px";     /* AR_20.00_5.00 */
+            self06.rs3padding1 = "padding:" + ((0.00617)*(viewWidth)+2.77778) + "px " + ((0.01235)*(viewWidth)+5.55556) + "px";    /* AR_10.00_5.00 */ /* AR_20.01_10.00 */
+            
+            // Sets mode of the "NAVIGATE" button
+            self06.display2 = false;
+            self06.morelessText2 = "NAVIGATE";
+            self06.moreless2 = function(){
+                if(self06.morelessText2 === "NAVIGATE"){
+                    self06.morelessText2 = "HIDE NAVIGATE";
+                    self06.display2 = true;
+                } 
+                else {
+                    self06.morelessText2 = "NAVIGATE";
+                    self06.display2 = false;
+                }
+            };
              
             compFactory.logView(06);
         }]
